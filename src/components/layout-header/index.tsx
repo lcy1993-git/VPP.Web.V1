998bb911a09ff5a37b4965c5edf5c7e2a81cb7da
lzq <1204263312@qq.com>
2024-04-02 11:50:38 +0800

    fix：header右侧数据接口

--- src/components/layout-header/index.tsx
+++ src/components/layout-header/index.tsx
@@ -1,19 +1,61 @@
 import logo from '@/assets/image/base/logo.png';
 import title from '@/assets/image/base/title.png';
+import { getRealtimeAlarm } from '@/services/devices-manage';
 import { BellFilled, LogoutOutlined, UserOutlined } from '@ant-design/icons';
-import { history } from '@umijs/max';
+import { history, useRequest } from '@umijs/max';
 import { useInterval } from 'ahooks';
 import { Badge, Button, Divider } from 'antd';
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './index.less';
 
 const LayoutHeader = () => {
   const [currentTime, setCurrentTime] = useState(dayjs().format('YYYY年MM月DD日 HH:MM:ss'));
+  // 告警数量
+  const [alarmNum, setAlarmNum] = useState<number | string>(0);
+
+  // 请求告警信息
+  const { run: fetchRealtimeAlarm } = useRequest(getRealtimeAlarm, {
+    manual: true,
+    onSuccess: (result: any) => {
+      if (result && parseInt(result.totalCount)) {
+        setAlarmNum(parseInt(result.totalCount));
+        // 播放音频
+        localStorage.setItem('whetherPlay', '1');
+      } else {
+        setAlarmNum(0);
+      }
+    },
+  });
 
   useInterval(() => {
     setCurrentTime(dayjs().format('YYYY年MM月DD日 HH:mm:ss'));
   }, 1000);
+
+  const clear = useInterval(
+    () => {
+      if (localStorage.getItem('token')) {
+        fetchRealtimeAlarm({
+          pageNum: 1,
+          pageSize: 10,
+          beginTime: dayjs(dayjs(new Date().getFullYear())).format('YYYY-MM-DD'),
+          endTime: dayjs(
+            dayjs(
+              `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
+            ),
+          ).format('YYYY-MM-DD'),
+        });
+      }
+    },
+    1000 * 60 * 5,
+    { immediate: true },
+  );
+
+  useEffect(() => {
+    return () => {
+      clear();
+    };
+  }, []);
 
   // 退出登录
   const logout = () => {
@@ -36,17 +78,28 @@ const LayoutHeader = () => {
         <img src={title} alt="成都市龙泉驿虚拟电厂管理平台" />
       </div>
       <div className={styles.headerRight}>
-        <span className={styles.currentTime}>{currentTime}</span>
+        <span className={`${styles.currentTime} ${styles.textStyle}`}>{currentTime}</span>
         <Divider type="vertical" className={styles.divider} />
-        <Badge count={100} size="small">
-          <Button size="small" type="text" icon={<BellFilled />} />
+        <Badge count={alarmNum} size="small" offset={[-5, 5]} style={{ color: '#fff' }}>
+          <Button
+            type="text"
+            className={styles.textStyle}
+            icon={<BellFilled />}
+            onClick={() => history.push('/alarm-manage/realtime-alarm')}
+          />
         </Badge>
         <Divider type="vertical" className={styles.divider} />
-        <Button size="small" type="text" icon={<UserOutlined />}>
+        <Button size="small" type="text" icon={<UserOutlined />} className={styles.textStyle}>
           {localStorage.getItem('userName') || '暂无身份'}
         </Button>
         <Divider type="vertical" className={styles.divider} />
-        <Button size="small" type="text" icon={<LogoutOutlined />} onClick={logout}>
+        <Button
+          size="small"
+          type="text"
+          icon={<LogoutOutlined />}
+          onClick={logout}
+          className={styles.textStyle}
+        >
           退出
         </Button>
       </div>

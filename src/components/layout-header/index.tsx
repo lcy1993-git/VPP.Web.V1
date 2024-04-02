749804ced753593ee2fe579bb993ad14fe651c27
lzq <1204263312@qq.com>
2024-04-02 10:24:09 +0800

    fix：用户角色

--- src/components/layout-header/index.tsx
+++ src/components/layout-header/index.tsx
@@ -1,16 +1,14 @@
-import styles from './index.less'
-import logo from '@/assets/image/base/logo.png'
-import title from '@/assets/image/base/title.png'
-import dayjs from 'dayjs';
-import { Badge, Button, Divider, Space } from 'antd'
-import { BellFilled, BellOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons'
-import { useState } from 'react'
-import { useInterval } from 'ahooks'
+import logo from '@/assets/image/base/logo.png';
+import title from '@/assets/image/base/title.png';
+import { BellFilled, LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { history } from '@umijs/max';
-
+import { useInterval } from 'ahooks';
+import { Badge, Button, Divider } from 'antd';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import styles from './index.less';
 
 const LayoutHeader = () => {
-
   const [currentTime, setCurrentTime] = useState(dayjs().format('YYYY年MM月DD日 HH:MM:ss'));
 
   useInterval(() => {
@@ -27,35 +25,32 @@ const LayoutHeader = () => {
     // 关闭音频
     localStorage.setItem('whetherPlay', '0');
     localStorage.setItem('closeAudio', 'false');
-  }
+  };
 
-
-  return <div className={styles.pageHeader}>
-    <div className={styles.headerLeft}>
-      <img src={logo} alt="同华" />
-    </div>
-    <div className={styles.headerMiddle} onClick={() => history.push('/big-screen/feature')}>
-      <img src={title} alt="成都市龙泉驿虚拟电厂管理平台" />
-    </div>
-    <div className={styles.headerRight}>
+  return (
+    <div className={styles.pageHeader}>
+      <div className={styles.headerLeft}>
+        <img src={logo} alt="同华" />
+      </div>
+      <div className={styles.headerMiddle} onClick={() => history.push('/big-screen/feature')}>
+        <img src={title} alt="成都市龙泉驿虚拟电厂管理平台" />
+      </div>
+      <div className={styles.headerRight}>
         <span className={styles.currentTime}>{currentTime}</span>
-        <Divider type="vertical" className={styles.divider}/>
+        <Divider type="vertical" className={styles.divider} />
         <Badge count={100} size="small">
-          <Button size="small" type="text" icon={<BellFilled />}/>
+          <Button size="small" type="text" icon={<BellFilled />} />
         </Badge>
-        <Divider type="vertical" className={styles.divider}/>
-        <Button size="small" type="text" icon={<UserOutlined />}>管理员</Button>
-        <Divider type="vertical" className={styles.divider}/>
-        <Button
-          size="small"
-          type="text"
-          icon={<LogoutOutlined />}
-          onClick={logout}
-        >
+        <Divider type="vertical" className={styles.divider} />
+        <Button size="small" type="text" icon={<UserOutlined />}>
+          {localStorage.getItem('userName') || '暂无身份'}
+        </Button>
+        <Divider type="vertical" className={styles.divider} />
+        <Button size="small" type="text" icon={<LogoutOutlined />} onClick={logout}>
           退出
         </Button>
+      </div>
     </div>
-  </div>
-}
-export default LayoutHeader
-
+  );
+};
+export default LayoutHeader;
